963226718b0751b5c4fd1b4562276edcc6b51cde
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-05-17 14:26:40 -0700

    added functions

--- smart file opener/modules/assistant_functions.py
+++ smart file opener/modules/assistant_functions.py
@@ -3,16 +3,42 @@ import shelve
 import subprocess
 import os
 
-from dictionaries import commands_decoder
+import logging
+from logging import debug
+logging.basicConfig(level=logging.DEBUG, format= '%(asctime)s - %(levelname)s - %(message)s')
+
+if __name__ == "__main__":
+    from dictionaries import commands_decoder
+else:
+    from modules.dictionaries import commands_decoder
+
+shelf_path = pathlib.Path(str(os.path.abspath("."))) / "smart file opener" / "modules" / "assistantVariables" / "local"
+debug(shelf_path)
+
+shelf_dict = shelve.open(str(shelf_path))
 
 def call_methods(commands:str):
     command_list = commands.split()
     if command_list[0] == commands_decoder.C_PLAY:
-        music(command_list[1:])
+        if play(command_list[1:]):
+            return True
 
-def music(args:list):
-    pass
+def play(args:list):
+    if args[1] == commands_decoder.SC_SONG:
+        if song(args):
+            return True
 
+def song(args:list):
+    try:
+        path = shelf_dict[args[2]]
+    except KeyError:
+        raise ValueError("Song in assistant functions threw a key error with key : " + str(args[2]))
+    process = subprocess.run(path)
+    if process.returncode == 0:
+        return True
 
 def add(key:str, value:str):
-    pass
+    global shelf_dict
+    shelf_dict[key] = value
+    shelf_dict.sync()
+
