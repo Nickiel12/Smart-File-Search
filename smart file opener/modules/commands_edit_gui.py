f69a65ceecdc0d35da0e16dfe6ca689e9c705b7e
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-05-20 21:24:40 -0700

    made file to create gui for editing commands

---
+++ smart file opener/modules/commands_edit_gui.py
@@ -0,0 +1,10 @@
+import shelve
+import wx
+
+import logging
+from logging import debug
+logging.basicConfig(level=logging.DEBUG,
+     format= '%(asctime)s - %(levelname)s - %(message)s')
+
+def run():
+    pass
